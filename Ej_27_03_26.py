c16531c37912ebbea7e2c60737eafd608a4960e9
Gerard-Martell <gerard.martell@anahuac.mx>
2026-03-27 09:39:39 -0600

    Add files via upload

--- Ej_27_03_26.py
+++ Ej_27_03_26.py
@@ -1,1 +1,37 @@
+# Listas - (Eqivalente a los Array)
 
+#Declaracion de listas
+#nombre = []
+numerosPrimos = [2,3,5,7,11,13]
+print(numerosPrimos)
+
+for i in range(0,len(numerosPrimos)):
+    print(numerosPrimos[i])
+
+diasSemana = ["Lunes", "Martes", "Mieroles"]
+print(diasSemana)
+
+for i in diasSemana:
+    print(i)
+
+fecha = [27,"Marzo",2026,"Viernes"]
+print(fecha)
+
+listaLista = [numerosPrimos, diasSemana, fecha, ["F",25]]
+print(listaLista)
+
+#Agregar valores a una lista
+vacaciones = []
+print(vacaciones)
+
+lugar = input("LUgar para vacacionar: ")
+vacaciones.append(lugar)
+print(vacaciones)
+
+calificaciones = []
+
+for i in range(6):
+    nota = int(input("Calificacion ("+str(i+1)+"): "))
+    calificaciones.append(nota)
+
+    print(calificaciones)
